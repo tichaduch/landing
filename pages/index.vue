12cddf1e10db9d7e8a7d854f3ab8ca8a2b2d7513
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2024-11-22 10:46:06 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 
   <client-only>
-    <v-sheet :width="psiWidth" :class="`position-fixed right-0 ${psiClass} mr-4`" color="transparent">
+    <v-sheet :width="psiWidth" max-height="80vh" :class="`position-fixed right-0 ${psiClass} mr-4`" color="transparent">
       <my-psi-animated :svg-to-display="svgToDisplay" />
     </v-sheet>
   </client-only>
@@ -20,11 +20,13 @@
 
   <v-card class="mt-n8 pa-4" tonal elevation="6" color="white" height="120vh" rounded="xl">
     test
+    {{ mdAndUp }}
   </v-card>
 
-  <v-sheet color="background" class="mt-n8" :height="mdAndUp ? '200px' : '100px'">
-
-  </v-sheet>
+  <client-only>
+    <v-sheet color="background" class="mt-n8" :height="mdAndUp ? 200 : 100">
+    </v-sheet>
+  </client-only>
 </template>
 
 <script lang="ts">
@@ -58,7 +60,7 @@ export default defineComponent({
     const layout = useLayout();
 
     const psiWidth = computed(() => {
-      if (display.mdAndUp.value) {
+      if (display.smAndUp.value) {
         return '20vw';
       }
 
@@ -69,7 +71,7 @@ export default defineComponent({
       switch (svgToDisplay.value) {
         case 'psi':
           if (display.mdAndUp.value) {
-            return 'mt-10';
+            return 'bottom-0';
           }
 
           return 'bottom-0 mb-n3';
