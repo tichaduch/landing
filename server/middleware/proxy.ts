2eb371904e4958d1dd7df186c017de6db4760edc
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2024-11-26 16:23:57 +0300

    upd

--- server/middleware/proxy.ts
+++ server/middleware/proxy.ts
@@ -1,4 +1,8 @@
 export default defineEventHandler((event) => {
+
+  /**
+   * Proxying all /api calls to api server
+   */
   if (event.path.startsWith('/api')) {
     const apiProxy = useRuntimeConfig(event).apiProxy || 'http://localhost:3000';
     return proxyRequest(event, `${apiProxy}${event.path}`);
